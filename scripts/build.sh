8c8e883914315c7ee502c529957235f87c6c156b
sugarshin <shinsugar@gmail.com>
2016-05-09 02:02:13 +0900

    fix build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,6 @@ CNAME=log.sugarshin.net
 
 npm run clean:build && \
 npm run webpack && \
-MAIN_SCRIPT=/assets/$(basename `ls build/assets/app-*.js`) npm run pug:build
-node scripts/articles -o build
+MAIN_SCRIPT=/assets/$(basename `ls build/assets/app-*.js`) npm run pug:build && \
+node scripts/articles -o build && \
 echo $CNAME > build/CNAME
