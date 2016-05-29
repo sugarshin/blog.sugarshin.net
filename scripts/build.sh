3475debaf67f2301bc4ac4d87f28507fba3313e0
sugarshin <shinsugar@gmail.com>
2016-05-29 11:37:49 +0900

    edit build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,8 @@ CNAME=log.sugarshin.net
 
 npm run clean:build && \
 npm run webpack && \
+npm run cp:build && \
 MAIN_SCRIPT=/assets/$(basename `ls build/assets/app-*.js`) npm run pug:build && \
 node scripts/articles -o build && \
-echo $CNAME > build/CNAME
+echo $CNAME > build/CNAME && \
+touch build/.nojekyll
