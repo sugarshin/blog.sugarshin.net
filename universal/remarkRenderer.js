620fdf31854ed06cbbae95beaf465b85d0f1359f
sugarshin <shinsugar@gmail.com>
2016-06-26 19:10:27 +0900

    hotfix remark-highlight.js

--- universal/remarkRenderer.js
+++ universal/remarkRenderer.js
@@ -2,10 +2,10 @@ const remark = require('remark');
 const html = require('remark-html');
 const highlightjs = require('remark-highlight.js');
 const slug = require('remark-slug');
-// const yamlConfig = require('remark-yaml-config');
+const yamlConfig = require('remark-yaml-config');
 
 module.exports = remark()
   .use(html)
   .use(highlightjs)
-  .use(slug);
-  // .use(yamlConfig);
+  .use(slug)
+  .use(yamlConfig);
