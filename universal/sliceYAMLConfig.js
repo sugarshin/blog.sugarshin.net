06492e0eecd21e2d31bcce37551b97eac9605db4
sugarshin <shinsugar@gmail.com>
2016-07-09 22:19:08 +0900

    add universal/sliceYAMLConfig.js

---
+++ universal/sliceYAMLConfig.js
@@ -0,0 +1,11 @@
+/**
+ * @param {String} markdown
+ * @param {String} [divider]
+ * @returns {String} yaml
+ */
+module.exports = function sliceYAMLConfig(markdown, divider = '---') {
+  const rows = markdown.split('\n');
+  const [firstLineIndex, secondLinesIndex] =
+    rows.reduce((result, row, i) => row === divider ? [...result, i] : result, []);
+  return rows.slice(firstLineIndex, secondLinesIndex).join('\n');
+}
