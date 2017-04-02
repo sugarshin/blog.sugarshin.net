66f31710b22fdc9696db0b2a0cd5667563a524ed
sugarshin <shinsugar@gmail.com>
2017-04-03 02:11:03 +0900

    Refactor Markdown renderer

--- src/components/ArticleBody/index.js
+++ src/components/ArticleBody/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import classnames from 'classnames';
-import markdownRenderer from '../../../universal/markdownRenderer';
-import 'github-markdown-css/github-markdown.css';
-import styles from './index.styl';
+import React from 'react'
+import classnames from 'classnames'
+import markdownRenderer from '../../../universal/markdownRenderer'
+import 'github-markdown-css/github-markdown.css'
+import styles from './index.styl'
 
 export default function ArticleBody({ markdown }) {
   return (
-    // eslint-disable-next-line react/no-danger
-    <div className={classnames('markdown-body', styles.body)} dangerouslySetInnerHTML={{
-      __html: markdownRenderer.processSync(markdown).toString()
-    }}></div>
-  );
+    <div className={classnames('markdown-body', styles.body)}>
+      {markdownRenderer.toReact(markdown)}
+    </div>
+  )
 }
--- universal/markdownRenderer.js
+++ universal/markdownRenderer.js
@@ -1,17 +1,41 @@
-const unified = require('unified');
-const parse = require('remark-parse');
-const slug = require('remark-slug');
-const toc = require('remark-toc');
-const yamlConfig = require('remark-yaml-config');
-const remarkToRehype = require('remark-rehype');
-const highlight = require('rehype-highlight');
-const stringify = require('rehype-stringify');
+const merge = require('lodash/merge')
+const unified = require('unified')
+const parse = require('remark-parse')
+const slug = require('remark-slug')
+const toc = require('remark-toc')
+const yamlConfig = require('remark-yaml-config')
+const remarkToRehype = require('remark-rehype')
+const highlight = require('rehype-highlight')
+const reactRenderer = require('remark-react')
+const reactLowlight = require('remark-react-lowlight').default
+const stringify = require('rehype-stringify')
+const sanitizeGitHubSchema = require('hast-util-sanitize/lib/github.json')
+const js = require('highlight.js/lib/languages/javascript')
+const bash = require('highlight.js/lib/languages/bash')
+const json = require('highlight.js/lib/languages/json')
 
-module.exports = unified()
-  .use(parse)
-  .use(slug)
-  .use(toc)
-  .use(yamlConfig)
+const basePlugins = [
+  parse,
+  slug,
+  toc,
+  yamlConfig,
+]
+
+const sanitize = merge(sanitizeGitHubSchema, { clobberPrefix: '', attributes: { code: ['className'] } })
+const reactComponentRenderer = unified().use(basePlugins).use(reactRenderer, {
+  sanitize,
+  remarkReactComponents: {
+    code: reactLowlight({
+      js, json, bash
+    })
+  }
+})
+const renderer = unified().use(basePlugins)
   .use(remarkToRehype)
-  .use(highlight)
-  .use(stringify);
+  .use(highlight) // not required
+  .use(stringify)
+
+module.exports = {
+  toReact: value => reactComponentRenderer.processSync(value).contents,
+  toString: value => renderer.processSync(value).toString()
+}
