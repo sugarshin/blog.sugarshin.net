fca131f41467f64f8ff0eae14ee60b1525fe16a3
sugarshin <shinsugar@gmail.com>
2017-04-09 00:00:18 +0900

    Refactor

--- scripts/helpers/writeFilePromisify.js
+++ scripts/helpers/writeFilePromisify.js
@@ -3,12 +3,13 @@ const fs = require('fs')
 module.exports = function writeFilePromisify(filePath, data) {
   return new Promise((resolve, reject) => {
     fs.writeFile(filePath, data, 'utf8', err => {
-      console.log(`Error: writeFilePromisify:\n${err}`)
-      console.log(`Success ${filePath} !`)
       if (err) {
+        console.log(`Error: writeFilePromisify:\n${err}`)
         reject(err)
+      } else {
+        console.log(`Success ${filePath} !`)
+        resolve()
       }
-      resolve()
     })
   })
 }
