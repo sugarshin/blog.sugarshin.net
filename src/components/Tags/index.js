63062fd7b0c4e913c78b34252e99029e7ac2c451
sugarshin <shinsugar@gmail.com>
2017-04-12 23:20:49 +0900

    Tree shaking

--- src/components/Tags/index.js
+++ src/components/Tags/index.js
@@ -3,5 +3,5 @@ import Tag from 'components/Tag'
 import styles from './index.styl'
 
 export default function Tags({ values }) {
-  return <ul className={styles.root}>{values.map(v => <li className={styles.item}><Tag value={v} /></li>)}</ul>
+  return <ul className={styles.root}>{values.map(v => <li key={v} className={styles.item}><Tag value={v} /></li>)}</ul>
 }
