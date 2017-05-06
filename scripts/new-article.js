df265a745f765d108362ff09e688004449055d9a
sugarshin <shinsugar@gmail.com>
2017-05-06 16:18:51 +0900

    Refactor new-article script

--- scripts/new-article.js
+++ scripts/new-article.js
@@ -1,54 +1,38 @@
-const { writeFileSync } = require('fs');
-const argv = require('minimist')(process.argv.slice(2));
-const mkdirp = require('mkdirp');
-const yaml = require('js-yaml');
-const moment = require('moment');
-const { protocol, domain } = require('../config/settings');
+const { writeFileSync } = require('fs')
+const argv = require('minimist')(process.argv.slice(2))
+const mkdirp = require('mkdirp')
+const moment = require('moment')
+const articleTemplate = require('./helpers/articleTemplate')
+const { protocol, domain } = require('../config/settings')
 
-const name = argv.n || argv.name;
+const name = argv.n || argv.name
 
 if (!name) {
-  throw new Error('`npm run na -- -n title` or `npm run na -- --name title`');
+  throw new Error('`npm run na -- -n article-title` or `npm run na -- --name=article-title`')
 }
 
-const origin = `${protocol}//${domain}`;
-const HR = '---';
-const d = moment();
-const imagePath = `assets/images/${d.format('YYYY/MM/DD')}/${name}`;
-const ogImageContent = `${origin}/${imagePath}/main.png`;
-const ogp = yaml.safeDump({
-  ogp: {
-    og: {
-      image: {
-        src: ogImageContent
-      }
-    }
-  }
-}, { lineWidth: -1 });
-const header = [
-  HR,
-  'title: Article title here',
-  `date: ${d.format('YYYY-MM-DD HH:mm')}`,
-  'public: true',
-  'tags:',
-  ogp.replace(/'/g, '') // Remove quote for src string
-     .replace(/\n$/, ''),
-  HR,
-  '',
-  `![Main](${ogImageContent.replace(origin, '')})`,
-  '',
-  '${Introductory paragraph...}',
-  '',
-  '## Table of Contents',
-  '',
-  '## ${First content heading}',
-  '',
-  ''
-].join('\n');
+const origin = `${protocol}//${domain}`
+const d = moment()
+const imagePath = `assets/images/${d.format('YYYY/MM/DD')}/${name}`
+const ogImageContent = `${origin}/${imagePath}/main.png`
+const content = articleTemplate({
+  title: 'Article title here',
+  date: d.format('YYYY-MM-DD HH:mm'),
+  ogImageURL: ogImageContent,
+  body: [
+    `![Main](${ogImageContent.replace(origin, '')})`,
+    '',
+    '## Table of Contents',
+    '',
+    '## ${First content heading}',
+    '',
+    '',
+  ],
+})
 
-writeFileSync(`articles/${d.format('YYYY-MM-DD')}_${name}.md`, header);
-console.log(`Success write new article file !`);
+writeFileSync(`articles/${d.format('YYYY-MM-DD')}_${name}.md`, content)
+console.log(`Success write new article file !`)
 
-const imageDir = `articles/${imagePath}`;
-mkdirp.sync(imageDir);
-console.log(`Created ${imageDir} !`);
+const imageDir = `articles/${imagePath}`
+mkdirp.sync(imageDir)
+console.log(`Created ${imageDir} !`)
