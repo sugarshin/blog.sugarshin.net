56d94603873e762877944a9b0986e8b1252a6d1b
sugarshin <shinsugar@gmail.com>
2017-05-07 13:49:33 +0900

    Add OGP meta data

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,8 @@ npm run clean:build && \
 npm run webpack && \
 node scripts/imagemin && \
 node scripts/articles-index -o build && \
+# TODO
+mkdir -p build/assets/common && cp src/images/p.jpg build/assets/images/common/open-graph.jpg && \
 node scripts/favicons && \
 MAIN_SCRIPT=/assets/$(basename `ls build/assets/app-*.js`) MAIN_STYLESHEET=/assets/$(basename `ls build/assets/app-*.css`) npm run templates:build && \
 rm -rf ./favicons.html.tmp && \
