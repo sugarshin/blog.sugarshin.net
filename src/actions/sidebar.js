e34b3dfc9b051a4711797704c7da63c48c5b719b
sugarshin <shinsugar@gmail.com>
2017-06-12 23:15:01 +0900

    Use redux-actions

--- src/actions/sidebar.js
+++ src/actions/sidebar.js
@@ -1,23 +1,7 @@
+import { createAction } from 'redux-actions'
 import types from 'constants/ActionTypes'
 
-export const toggleSidebar = () => {
-  return {
-    type: types.TOGGLE_SIDEBAR,
-  }
-}
-
-export const openSidebar = () => {
-  return {
-    type: types.OPEN_SIDEBAR,
-  }
-}
-
-export const closeSidebar = () => {
-  return {
-    type: types.CLOSE_SIDEBAR,
-  }
-}
-
-export const toggleDocked = docked => {
-  return { type: types.TOGGLE_SIDEBAR_DOCKED, docked }
-}
+export const toggleSidebar = createAction(types.TOGGLE_SIDEBAR)
+export const openSidebar = createAction(types.OPEN_SIDEBAR)
+export const closeSidebar = createAction(types.CLOSE_SIDEBAR)
+export const toggleSidebarDocked = createAction(types.TOGGLE_SIDEBAR_DOCKED, docked => ({ docked }))
