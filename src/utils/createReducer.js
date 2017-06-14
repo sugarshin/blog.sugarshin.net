f3d11d682fbfd4e57297ee1735251a605b1082ce
sugarshin <shinsugar@gmail.com>
2017-06-14 23:55:10 +0900

    Delete unused code

--- src/utils/createReducer.js
+++
@@ -1,4 +0,0 @@
-export default function createReducer(initialState, handlers) {
-  return (state = initialState, action) => handlers.hasOwnProperty(action.type) ?
-    handlers[action.type](state, action) : state
-}
