1b31c2c4ecd8881e663b58fdf3d56357923b73cf
sugarshin <shinsugar@gmail.com>
2017-10-01 11:21:24 +0900

    Make deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+GIT_URL=$(git remote get-url origin)
+BRANCH=gh-pages
+COMMIT=$(git rev-parse --short HEAD)
+BUILD=build
+DIR=.deploy
+
+rm -rf $DIR
+git clone --depth=1 $GIT_URL -b $BRANCH $DIR || (git init $DIR && cd $DIR && git remote add origin $GIT_URL && git checkout -b $BRANCH)
+rm -rf ${DIR}/*
+cp -R ${DIR}/../${BUILD}/* $DIR
+cd $DIR
+git add --all
+git commit -m "Built artifacts of $COMMIT [ci skip]" || true
+git push origin $BRANCH
