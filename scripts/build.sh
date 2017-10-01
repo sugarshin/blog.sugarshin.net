38c4f3f9dcb4deadec0306c1708ba43240249882
sugarshin <shinsugar@gmail.com>
2017-10-01 16:17:27 +0900

    Code splitting

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ node scripts/articles-index -o $BUILD
 mkdir -p $BUILD/assets/images/common
 cp src/images/p.jpg $BUILD/assets/images/common/open-graph.jpg
 node scripts/favicons
-MAIN_SCRIPT=/assets/$(basename `ls $BUILD/assets/app-*.js`) MAIN_STYLESHEET=/assets/$(basename `ls $BUILD/assets/app-*.css`) npm run templates:build
+MAIN_SCRIPT=/assets/$(basename `ls $BUILD/assets/app-*.js`) MAIN_STYLESHEET=/assets/$(basename `ls $BUILD/assets/app-*.css`) VENDOR_SCRIPT=/assets/$(basename `ls $BUILD/assets/vendor-*.js`) VENDOR_STYLESHEET=/assets/$(basename `ls $BUILD/assets/vendor-*.css`) npm run templates:build
 rm -rf ./favicons.html.tmp
 node scripts/sitemap
 node scripts/feed
