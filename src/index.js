1b0c1dbc932efb618e9d7f677c6e1c040c845500
sugarshin <shinsugar@gmail.com>
2018-03-16 17:13:47 +0900

    Integrate LogRocket

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import 'stylus/index.styl'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Raven from 'raven-js'
+import LogRocket from 'logrocket'
+import setupLogRocketReact from 'logrocket-react'
 import noop from 'lodash/noop'
 import Root from 'containers/Root'
 import configureStore from 'store/configureStore'
@@ -19,6 +21,18 @@ const main = () => {
       process.env.SENTRY_DSN,
       { release: process.env.BUILD_NUMBER, debug: process.env.NODE_ENV !== 'production' }
     ).install()
+  }
+
+  if (process.env.LOGROCKET_APP_ID) {
+    LogRocket.init(process.env.LOGROCKET_APP_ID)
+    setupLogRocketReact(LogRocket)
+  }
+
+  if (process.env.SENTRY_DSN && process.env.LOGROCKET_APP_ID) {
+    Raven.setDataCallback(data => {
+      data.extra.sessionURL = LogRocket.sessionURL
+      return data
+    })
   }
 
   APIBase.baseURI = process.env.API_BASE
