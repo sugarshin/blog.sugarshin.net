5470ce4b51d48cd86c2a9fdcf06c0716f03c4695
sugarshin <shinsugar@gmail.com>
2018-05-13 10:25:39 +0900

    Use `disqus-react`

--- src/components/Disqus/index.js
+++ src/components/Disqus/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import DisqusComments from 'react-disqus-comments'
+import DisqusReact from 'disqus-react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import styles from './index.styl'
 
-const Disqus = ({ className, ...rest }) => (
+const Disqus = ({ className, shortname, identifier, title, url }) => (
   <div className={classnames(styles.root, className)}>
-    <DisqusComments {...rest} />
+    <DisqusReact.DiscussionEmbed shortname={shortname} config={{ identifier, title, url }} />
   </div>
 )
 
@@ -15,6 +15,7 @@ Disqus.propTypes = {
   identifier: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   shortname: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
   onNewComment: PropTypes.func,
 }
 
