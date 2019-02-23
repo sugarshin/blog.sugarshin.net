0302e61fc3bba3fca457f2240acd433b6c694c24
sugarshin <shinsugar@gmail.com>
2019-02-24 01:51:53 +0900

    Fix e2e test

--- e2e-test/test.js
+++ e2e-test/test.js
@@ -3,10 +3,13 @@ const puppeteer = require('puppeteer')
 const main = async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
   const page = await browser.newPage()
-  await page.goto('http://localhost:3000')
+  await page.goto('http://localhost:3000', { waitUntil: 'networkidle2' })
   await page.screenshot({ path: 'screenshot.png' })
 
   await browser.close()
 }
 
-main()
+main().catch(e => {
+  console.error(e)
+  process.exit(1)
+})
