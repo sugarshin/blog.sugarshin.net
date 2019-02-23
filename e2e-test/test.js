06280f77b1d4a03c64c52491ba8a73c606c1a54b
sugarshin <shinsugar@gmail.com>
2019-02-24 01:42:42 +0900

    Run puppeteer with '--no-sandbox', '--disable-setuid-sandbox'

--- e2e-test/test.js
+++ e2e-test/test.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
 const main = async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
   const page = await browser.newPage()
   await page.goto('http://localhost:3000')
   await page.screenshot({ path: 'screenshot.png' })
