602159a15cdcc48d053f78978d1e299a745a704f
sugarshin <shinsugar@gmail.com>
2019-06-02 10:20:21 +0900

    Remove DevTools

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -6,27 +6,17 @@ import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import Ribbon from 'react-github-fork-ribbon'
 import Routes from 'routes'
-// import DevTools from 'store/debugger/DevTools'
-
-// // https://github.com/gaearon/redux-devtools-dock-monitor/blob/master/src/actions.js#L1
-// const TOGGLE_VISIBILITY = '@@redux-devtools-log-monitor/TOGGLE_VISIBILITY'
-// let devTools
 
 const Root = ({ store, history }) => {
   return (
     <Provider store={store}>
       <div>
-        <Ribbon
-          color='black'
-          // onClick={() => devTools.liftedStore.dispatch({ type: TOGGLE_VISIBILITY })}
-        >
-          control + {'{h,p,m}'}
+        <Ribbon color='black'>
+          {process.env.NODE_ENV || 'development'}
         </Ribbon>
         <ConnectedRouter history={history}>
           <Routes />
         </ConnectedRouter>
-        {/* @see https://github.com/reduxjs/redux-devtools/issues/399 */}
-        {/* <DevTools ref={c => devTools = c} /> */}
       </div>
     </Provider>
   )
