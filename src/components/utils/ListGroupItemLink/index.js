01835ef295b8bd1a5347f6ed848df106cc801590
sugarshin <shinsugar@gmail.com>
2019-06-02 10:10:50 +0900

    Rename cx => classnames

--- src/components/utils/ListGroupItemLink/index.js
+++ src/components/utils/ListGroupItemLink/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import cx from 'classnames'
+import classnames from 'classnames'
 
 export default function ListGroupItemLink({ to, children }) {
   return (
     <Route
       path={to}
       children={({ match: active }) => ( // eslint-disable-line react/no-children-prop
-        <Link to={to} className={cx('list-group-item', { active })}>
+        <Link to={to} className={classnames('list-group-item', { active })}>
           {children}
         </Link>
       )}
