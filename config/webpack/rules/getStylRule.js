3fa9f232cbefe3ba4bb316ffa7286afdee45e8b2
Shingo Sato <shinsugar@gmail.com>
2019-08-14 22:17:15 +0900

    Update css-loader

--- config/webpack/rules/getStylRule.js
+++ config/webpack/rules/getStylRule.js
@@ -2,26 +2,20 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = prod => {
   const options = {
-    minimize: prod,
-    modules: true,
+    modules: {
+      localIdentName: prod ? '[hash:base64:32]' : '[path][name]__[local]___[hash:base64:8]',
+    },
     importLoaders: 2,
-    localIdentName: prod ? '[hash:base64:32]' : '[path][name]__[local]___[hash:base64:8]',
   }
-  const rule = { test: /\.styl$/ }
-  if (prod) {
-    rule.use = [
-      MiniCssExtractPlugin.loader,
+  const rule = {
+    test: /\.styl$/,
+    use: [
+      prod ? MiniCssExtractPlugin.loader : 'style-loader',
       { loader: 'css-loader', options },
       'postcss-loader',
       'stylus-loader',
-    ]
-  } else {
-    rule.use = [
-      'style-loader',
-      { loader: 'css-loader', options },
-      'postcss-loader',
-      'stylus-loader',
-    ]
+    ],
   }
+
   return rule
 }
