33a9eb4e8cb9e09a00ac3ab19f4102de76da2898
Shingo Sato <shinsugar@gmail.com>
2020-09-22 10:51:40 +0900

    Make parseYamlFrontmatter

--- markdown/index.js
+++ markdown/index.js
@@ -2,14 +2,12 @@ const parse = require('remark-parse')
 const frontmatter = require('remark-frontmatter')
 const slug = require('remark-slug')
 const toc = require('remark-toc')
-const yamlConfig = require('remark-yaml-config')
 const externalLinks = require('remark-external-links')
 
 const basePlugins = [
   parse,
   externalLinks,
   frontmatter,
-  yamlConfig,
   slug,
   toc,
 ]
