f68979ee3ce4928f30b4401cc1786ef171c140d1
Shingo Sato <shinsugar@gmail.com>
2021-06-11 06:20:10 +0900

    Add remark-gfm

--- markdown/index.js
+++ markdown/index.js
@@ -3,10 +3,12 @@ const frontmatter = require('remark-frontmatter')
 const slug = require('remark-slug')
 const toc = require('remark-toc')
 const externalLinks = require('remark-external-links')
+const gfm = require('remark-gfm')
 
 const basePlugins = [
   parse,
   externalLinks,
+  gfm,
   frontmatter,
   slug,
   [toc, { heading: '目次' }],
