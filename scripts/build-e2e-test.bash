d2eeaaf1efa8166e0b6eab009c2f06777119a8fe
Shingo Sato <shinsugar@gmail.com>
2021-09-20 08:08:06 +0900

    Detach LogRocket

--- scripts/build-e2e-test.bash
+++ scripts/build-e2e-test.bash
@@ -6,7 +6,7 @@ BUILD=build-e2e-test
 npm run clean:e2e-test
 npm run mkdir:e2e-test
 npm run articles-index:e2e-test
-unset API_BASE SEGMENT_WRITE_KEY LOGROCKET_APP_ID SENTRY_DSN; BUILD_DIR=$BUILD npm run webpack
+unset API_BASE SEGMENT_WRITE_KEY SENTRY_DSN; BUILD_DIR=$BUILD npm run webpack
 node scripts/copy-articles -o $BUILD
 npm run cp:e2e-test
 
