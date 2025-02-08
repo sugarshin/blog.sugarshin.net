61eab2651448dcf204cc027a36a923635220dc6c
Shingo Sato <shinsugar@gmail.com>
2025-02-08 14:00:16 +0900

    Not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,20 @@
-// import { AlertIcon } from '@primer/octicons-react';
 import Link from 'next/link';
-// import styles from './not-found.module.css';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>404 Notfound</h2>
-      <p>
-        <Link href="/">Return to top</Link>
-      </p>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">404 Not Found</h1>
+          <p className="py-6">
+            Oops, the page you&apos;re looking for doesn&apos;t exist. Please
+            continue to the top page.
+          </p>
+          <Link href="/" className="btn btn-primary">
+            Return to top
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
