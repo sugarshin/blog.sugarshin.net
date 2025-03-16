c46c7dd4bdc5debab28d4ef8ccf03a708d717eb3
Shingo Sato <shinsugar@gmail.com>
2025-03-16 22:32:54 +0900

    Update page.tsx

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -36,7 +36,7 @@ export default async function Page({
       </h1>
       <ArticleMeta meta={meta} />
       <div
-        className="markdown-body pt-4 border-t border-gray-200"
+        className="markdown-body p-4 border-t border-gray-200"
         id="article-content"
       >
         <Markdown>{md}</Markdown>
